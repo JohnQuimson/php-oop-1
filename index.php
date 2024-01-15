7c1890683b2af6c071216156e5ee820659636ba8
JohnQuimson <john.quimson777@gmail.com>
2024-01-15 21:19:54 +0100

    feat: aggiunta metodo

--- index.php
+++ index.php
@@ -5,11 +5,32 @@ class Movie
   public $titolo;
   public $anno;
   public $genere;
+  public $recente;
 
   public function __construct($_titolo, $_anno, $_genere)
   {
     $this->titolo = $_titolo;
     $this->anno = $_anno;
     $this->genere = $_genere;
+
+    // Richiamo la funzione
+    $this->isRecent();
+  }
+
+  // Funzione che verifica se è recente
+  public function isRecent()
+  {
+    if ($this->anno >= 2015) {
+      $this->recente = 'Sì';
+    } else {
+      $this->recente = 'No';
+    }
   }
 }
+
+// Stampa degli obj
+$film1 = new Movie('Titanic', 1997, 'Romantico/Avventura');
+var_dump($film1);
+
+$film2 = new Movie('Oppenheimer', 2023, 'Thriller');
+var_dump($film2);
