c74cb6b5335312c4a0b4ab322aa7e217b5d39f8c
JohnQuimson <john.quimson777@gmail.com>
2024-01-15 19:10:27 +0100

    feat: construct

--- index.php
+++ index.php
@@ -5,4 +5,11 @@ class Movie
   public $titolo;
   public $anno;
   public $genere;
+
+  public function __construct($_titolo, $_anno, $_genere)
+  {
+    $this->titolo = $_titolo;
+    $this->anno = $_anno;
+    $this->genere = $_genere;
+  }
 }
